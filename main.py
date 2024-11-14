15ede57deb1215411d076ff66ad0f95c7bed6538
jisong017 <92095483+jisong017@users.noreply.github.com>
2024-11-14 19:35:35 +0900

    Add initial scripts in js directory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, status
-from router import auth, project
+from router import auth, project, retrospects, templates
 
 
 app = FastAPI()
@@ -9,6 +9,13 @@ app.include_router(auth.router)
 
 # 프로젝트 라우터
 app.include_router(project.router)
+
+# 회고 라우터
+app.include_router(retrospects.router)
+
+# 탬플릿 라우터
+app.include_router(templates.router)
+
 
 # API Server Test
 @app.get("/", status_code=status.HTTP_200_OK)
