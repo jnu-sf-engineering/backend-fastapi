7b1b294d95eb1b0d5f26e162ca87fc5da6b6be23
Park Kyeong Jun <100195725+Kyeong6@users.noreply.github.com>
2024-11-25 15:46:14 +0900

    feat: JWT 방식 API 적용 및 API 명세서 변경(네이밍 카멜방식으로 변경)으로 인한 수정사항 반영

--- router/project.py
+++ router/project.py
@@ -11,19 +11,14 @@ router = APIRouter(prefix="/v1/project", tags=["프로젝트"])
 
 # 프로젝트 생성/수정/삭제 요청 검증
 class ProjectRequest(BaseModel):
-    project_name: str
+    projectName: str
     manager: str
-
-
-# 프로젝트 조회 요청 검증
-class ProjectListRequest(BaseModel):
-    user_id: int
 
 # 프로젝트 조회 요청 검증
 class ProjectListResponse(BaseModel):
-    project_id: int
-    project_name: str
-    sprint_count: int
+    projectId: int
+    projectName: str
+    sprintCount: int
     manager: str
 
 
@@ -40,12 +35,21 @@ def get_user_from_token(authorization: str = Header(...)):
     return payload.get("user_id")  # 토큰에서 user_id 추출
 
 
+# 프로젝트 소유 여부 로직
+def validate_project_access(user_id: int, projectId: int, db: Session):
+    project = db.query(Project).filter(
+        Project.PROJECT_ID == projectId,
+        Project.USER_ID == user_id
+    ).first()
+
+    if not project:
+        raise ProjectNotFound()
+    return project
+
+
 # 프로젝트 목록 조회
 @router.get("/", response_model=dict)
-async def get_projects(request: ProjectListRequest, db: Session = Depends(get_db)):
-    
-    # user_id 검증
-    user_id = request.user_id
+async def get_projects(user_id: int = Depends(get_user_from_token), db: Session = Depends(get_db)):
 
     # 프로젝트 조회
     projects = db.query(Project).filter(Project.USER_ID == user_id).all()
@@ -57,9 +61,9 @@ async def get_projects(request: ProjectListRequest, db: Session = Depends(get_db
     # 반환값 구성
     response = [
         {
-            "project_id": project.PROJECT_ID,
-            "project_name": project.PROJECT_NAME,
-            "sprint_count": project.SPRINT_COUNT,
+            "projectId": project.PROJECT_ID,
+            "projectName": project.PROJECT_NAME,
+            "sprintCount": project.SPRINT_COUNT,
             "manager": project.MANAGER
         }
         for project in projects
@@ -76,22 +80,23 @@ async def get_projects(request: ProjectListRequest, db: Session = Depends(get_db
 
 # 프로젝트 생성
 @router.post("/", response_model=dict)
-async def create_project(request: ProjectRequest, user_id: int = Depends(get_user_from_token), 
+async def create_project(request: ProjectRequest,
+                         user_id: int = Depends(get_user_from_token), 
                          db: Session = Depends(get_db)):
     
     # 프로젝트 생성 시 필드 누락 검증
-    if not request.project_name or not request.manager:
-        raise MissingFieldData(['project_name', "manager"])
+    if not request.projectName or not request.manager:
+        raise MissingFieldData(['projectName', "manager"])
     
     # 프로젝트명 중복 확인
-    duplcate_project = db.query(Project).filter(Project.PROJECT_NAME == request.project_name, 
+    duplcate_project = db.query(Project).filter(Project.PROJECT_NAME == request.projectName, 
                                                 Project.USER_ID == user_id).first()
     if duplcate_project:
         raise DuplicateProjectName()
 
     # 프로젝트 생성 후 데이터베이스 적재
     new_project = Project(
-        PROJECT_NAME=request.project_name, MANAGER=request.manager, 
+        PROJECT_NAME=request.projectName, MANAGER=request.manager, 
         USER_ID=user_id)
     db.add(new_project)
     db.commit()
@@ -101,7 +106,7 @@ async def create_project(request: ProjectRequest, user_id: int = Depends(get_use
     response = {
         "success": True,
         "response": {
-            "project_id": new_project.PROJECT_ID
+            "projectId": new_project.PROJECT_ID
         },
         "error": None
     }
@@ -110,25 +115,22 @@ async def create_project(request: ProjectRequest, user_id: int = Depends(get_use
 
 
 # 프로젝트 수정
-@router.put("/{project_id}", response_model=dict)
-async def update_project(project_id: int, request: ProjectRequest, user_id: int = Depends(get_user_from_token), 
+@router.put("/{projectId}", response_model=dict)
+async def update_project(projectId: int,
+                         request: ProjectRequest,
+                         user_id: int = Depends(get_user_from_token), 
                          db: Session = Depends(get_db)):
     
-    # path parameter에 해당하는 프로젝트 조회
-    project = db.query(Project).filter(Project.PROJECT_ID == project_id, 
-                                       Project.USER_ID == user_id).first()
-
-    # 해당하는 프로젝트 존재 하지 않을 경우 로직
-    if not project:
-        raise ProjectNotFound()
+    # 프로젝트 소유 여부 확인
+    project = validate_project_access(user_id, projectId, db)
     
     # 기존 프로젝트 데이터와 동일할 경우 예외 처리
-    if project.PROJECT_NAME == request.project_name and project.MANAGER == request.manager:
+    if project.PROJECT_NAME == request.projectName and project.MANAGER == request.manager:
         raise DuplicateProjectData()
     
     
     # 프로젝트 정보 수정 후 업데이트
-    project.PROJECT_NAME = request.project_name
+    project.PROJECT_NAME = request.projectName
     project.MANAGER = request.manager
     db.commit()
     db.refresh(project)
@@ -137,7 +139,7 @@ async def update_project(project_id: int, request: ProjectRequest, user_id: int 
     response = {
         "success": True,
         "response": {
-            "project_id": project.PROJECT_ID
+            "projectId": project.PROJECT_ID
         },
         "error": None
     }
@@ -146,17 +148,13 @@ async def update_project(project_id: int, request: ProjectRequest, user_id: int 
 
 
 # 프로젝트 삭제
-@router.delete("/{project_id}", response_model=dict)
-async def delete_project(project_id: int, user_id: int = Depends(get_user_from_token), 
+@router.delete("/{projectId}", response_model=dict)
+async def delete_project(projectId: int,
+                         user_id: int = Depends(get_user_from_token), 
                          db: Session = Depends(get_db)):
     
-    # path parameter에 해당하는 프로젝트 조회
-    project = db.query(Project).filter(Project.PROJECT_ID == project_id, 
-                                       Project.USER_ID == user_id).first()
-
-    # 해당하는 프로젝트 존재 하지 않을 경우 로직
-    if not project:
-        raise ProjectNotFound()
+    # 프로젝트 소유 여부 확인
+    project = validate_project_access(user_id, projectId, db)
     
     # 프로젝트 정보 삭제 
     db.delete(project)
