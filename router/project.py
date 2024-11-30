e99d10d3562f1245d77d0955a11442ec68b775d0
Park Kyeong Jun <100195725+Kyeong6@users.noreply.github.com>
2024-11-30 12:45:51 +0900

    fix: secret key를 이용한 인코딩방식 변경으로 Spring 디코딩 에러 해결

--- router/project.py
+++ router/project.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from db.database import get_db
 from db.models import Project
 from error.exceptions import MissingFieldData, DuplicateProjectName, ProjectNotFound, DuplicateProjectData
-from router.token_decode import decode_access_token
+from router.token_decode import get_user_id_from_token
 
 # 프로젝트 라우터
 router = APIRouter(prefix="/v1/project", tags=["프로젝트"])
@@ -21,20 +21,6 @@ class ProjectListResponse(BaseModel):
     sprintCount: int
     manager: str
 
-
-# Authorization 헤더에서 토큰 추출 및 디코딩
-def get_user_from_token(authorization: str = Header(...)):
-    # Bearer 토큰에서 실제 토큰 값 추출
-    if not authorization.startswith("Bearer "):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authorization header",
-        )
-    token = authorization.split(" ")[1]
-    payload = decode_access_token(token)
-    return payload.get("user_id")  # 토큰에서 user_id 추출
-
-
 # 프로젝트 소유 여부 로직
 def validate_project_access(user_id: int, projectId: int, db: Session):
     project = db.query(Project).filter(
@@ -49,7 +35,7 @@ def validate_project_access(user_id: int, projectId: int, db: Session):
 
 # 프로젝트 목록 조회
 @router.get("/", response_model=dict)
-async def get_projects(user_id: int = Depends(get_user_from_token), db: Session = Depends(get_db)):
+async def get_projects(user_id: int = Depends(get_user_id_from_token), db: Session = Depends(get_db)):
 
     # 프로젝트 조회
     projects = db.query(Project).filter(Project.USER_ID == user_id).all()
@@ -81,7 +67,7 @@ async def get_projects(user_id: int = Depends(get_user_from_token), db: Session 
 # 프로젝트 생성
 @router.post("/", response_model=dict)
 async def create_project(request: ProjectRequest,
-                         user_id: int = Depends(get_user_from_token), 
+                         user_id: int = Depends(get_user_id_from_token), 
                          db: Session = Depends(get_db)):
     
     # 프로젝트 생성 시 필드 누락 검증
@@ -118,7 +104,7 @@ async def create_project(request: ProjectRequest,
 @router.put("/{projectId}", response_model=dict)
 async def update_project(projectId: int,
                          request: ProjectRequest,
-                         user_id: int = Depends(get_user_from_token), 
+                         user_id: int = Depends(get_user_id_from_token), 
                          db: Session = Depends(get_db)):
     
     # 프로젝트 소유 여부 확인
@@ -150,7 +136,7 @@ async def update_project(projectId: int,
 # 프로젝트 삭제
 @router.delete("/{projectId}", response_model=dict)
 async def delete_project(projectId: int,
-                         user_id: int = Depends(get_user_from_token), 
+                         user_id: int = Depends(get_user_id_from_token), 
                          db: Session = Depends(get_db)):
     
     # 프로젝트 소유 여부 확인
